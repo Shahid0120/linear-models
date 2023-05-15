31fcfae093fedfd4788e263d92d35a59cbc97d5b
Shahid-Developer <shahid.hussain0120@gmail.com>
2023-05-15 12:15:55 +1000

    created a packages install file

---
+++ R_tutorial/first_package.R
@@ -0,0 +1,8 @@
+install.packages("pacman")
+require(pacman)
+library(pacman)
+
+pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, ggvis, httr, lubridate,
+               plotly, rio, rmarkfown, kshiny, stringr, tidyr)
+
+p_unload(all)
